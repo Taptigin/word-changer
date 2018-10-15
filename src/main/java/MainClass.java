65c28c53a27595b2d6de556e75843ec4687457e0
Seliverstow Alexandr <a.v.seliverstow@gmail.com>
2018-10-15 21:13:03 +0400

    123

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,14 +1,17 @@
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  * Created by Александр on 15.10.2018.
@@ -32,18 +35,67 @@ public class MainClass {
             // открываем файл и считываем его содержимое в объект XWPFDocument
             docxFile = new XWPFDocument(OPCPackage.open(fileInputStream));
 
-            XWPFWordExtractor extractor = new XWPFWordExtractor(docxFile);
-            System.out.println(extractor.getText());
-        } catch (Exception e){
+            String fromText = getFromText();
+            String toText = getToText();
+            changeText(fromText, toText);
+
+            //XWPFWordExtractor extractor = new XWPFWordExtractor(docxFile);
+            //System.out.println(extractor.getText());
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
 
     }
 
-    private void setPathFile() throws IOException{
+    private String getFromText() throws Exception {
+        System.out.println("Введите заменяемый текст");
+        return consoleReader.readLine();
+    }
+
+    private String getToText() throws Exception {
+        System.out.println("Введите текст На который нужно заменить");
+        return consoleReader.readLine();
+    }
+
+    private void setPathFile() throws IOException {
         System.out.println("Введите путь к файлу.");
         path = consoleReader.readLine();
+    }
+
+    private void changeText(String fromText, String toText) {
+        for (XWPFParagraph p : docxFile.getParagraphs()) {
+            List<XWPFRun> runs = p.getRuns();
+            if (runs != null) {
+                for (XWPFRun r : runs) {
+                    String text = r.getText(0);
+                    if (text != null && text.contains(fromText)) {
+                        text = text.replace(fromText, toText);
+                        r.setText(text, 0);
+                    }
+                }
+            }
+        }
+        for (XWPFTable tbl : docxFile.getTables()) {
+            for (XWPFTableRow row : tbl.getRows()) {
+                for (XWPFTableCell cell : row.getTableCells()) {
+                    for (XWPFParagraph p : cell.getParagraphs()) {
+                        for (XWPFRun r : p.getRuns()) {
+                            String text = r.getText(0);
+                            if (text != null && text.contains(fromText)) {
+                                text = text.replace(fromText, toText);
+                                r.setText(text, 0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        try {
+            docxFile.write(new FileOutputStream(path));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
